ca46bdbe4a718e28ed28b61cd933e67fb0d5beef
wjmellon <wmellon@asu.edu>
2022-08-09 17:06:42 -0700

    smush together trees

--- CladeTree.R
+++ CladeTree.R
@@ -1,14 +1,15 @@
 #open libraries
 library(tidyverse)
 library(phytools)
+library(patchwork)
+library(tidyverse)
+library(cowplot)
 
+
+
+par(mfrow=c(3,1))
 #read tree
 tree <- read.tree("min20Fixed516.nwk")
-tree
-plot(tree)
-
-## plot tree
-plotTree(tree,type="fan", ftype = "i")
 
 ## read csv file for mammals
 
@@ -16,7 +17,6 @@ Data<-read.csv("min20516.csv",header = TRUE,row.names=9)
 Data<- filter(Data, is.element(Clade, c("Mammalia")))
 row.names(Data)= gsub(" ", "_", row.names(Data))
 
-plot(tree)
 
 ## prune the tree to match the data
 includedSpecies <- row.names(Data)
@@ -34,14 +34,10 @@ pruned.tree<-drop.tip(
 pruned.tree <- keep.tip(pruned.tree,pruned.tree$tip.label)
 
 
-plot(pruned.tree)
-
 ## removing discrepancies
 Data$Keep <- row.names(Data) %in% pruned.tree$tip.label
 Data <- Data[!(Data$Keep==FALSE),]
 
-
-plotTree(pruned.tree,type="fan",ftype="i")
 
 ## match species from csv to the tree
 neoplasia=dplyr::filter(Data, row.names(Data) %in% pruned.tree$tip.label)
@@ -55,22 +51,22 @@ neoplasia
 
 ## estimate ancestral states
 fit<-fastAnc(pruned.tree,neoplasia,vars=TRUE,CI=TRUE)
-fit
 
 ## confidence intervals
 fit$CI[1,]
 
 ## projection of the reconstruction onto the edges of the tree
-Mobj<-contMap(pruned.tree,neoplasia,plot=TRUE,res=200, type = "fan")
+Mobj<-contMap(pruned.tree,neoplasia,plot=FALSE,res=300, type = "phylogram",lwd = 2)
+
 
 ## invert color scale so red is highest trait value and blue is lowest
 Mobj<-setMap(Mobj,invert=TRUE)
 
+
 ## plot 
 m<-plot(Mobj,
-     fsize=c(0.4,0.8),leg.txt="neoplasia prevalence")
-
-
+     fsize=c(1,0.8),leg.txt="neoplasia prevalence",lwd= 2.5)
+title(main="A", adj = .05, line = -1)
 
 
 
@@ -80,7 +76,6 @@ Data<-read.csv("min20516.csv",header = TRUE,row.names=9)
 Data<- filter(Data, is.element(Clade, c("Sauropsida")))
 row.names(Data)= gsub(" ", "_", row.names(Data))
 
-plot(tree)
 
 ## prune the tree to match the data
 includedSpecies <- row.names(Data)
@@ -98,14 +93,10 @@ pruned.tree<-drop.tip(
 pruned.tree <- keep.tip(pruned.tree,pruned.tree$tip.label)
 
 
-plot(pruned.tree)
-
 ## removing discrepancies
 Data$Keep <- row.names(Data) %in% pruned.tree$tip.label
 Data <- Data[!(Data$Keep==FALSE),]
 
-
-plotTree(pruned.tree,type="fan",ftype="i")
 
 ## match species from csv to the tree
 neoplasia=dplyr::filter(Data, row.names(Data) %in% pruned.tree$tip.label)
@@ -125,14 +116,15 @@ fit
 fit$CI[1,]
 
 ## projection of the reconstruction onto the edges of the tree
-Sobj<-contMap(pruned.tree,neoplasia,plot=TRUE,res=200, type = "fan")
+Sobj<-contMap(pruned.tree,neoplasia,plot=FALSE,res=200, type = "fan")
 
 ## invert color scale so red is highest trait value and blue is lowest
 Sobj<-setMap(Sobj,invert=TRUE)
 
 ## plot 
 s<-plot(Sobj,
-           fsize=c(0.4,0.8),leg.txt="neoplasia prevalence")
+           fsize=c(1,0.8),leg.txt="neoplasia prevalence",lwd = 2.5)
+title(main="B", adj = .05, line = -1)
 
 
 
@@ -144,7 +136,6 @@ Data<-read.csv("min20516.csv",header = TRUE,row.names=9)
 Data<- filter(Data, is.element(Clade, c("Amphibia")))
 row.names(Data)= gsub(" ", "_", row.names(Data))
 
-plot(tree)
 
 ## prune the tree to match the data
 includedSpecies <- row.names(Data)
@@ -162,14 +153,10 @@ pruned.tree<-drop.tip(
 pruned.tree <- keep.tip(pruned.tree,pruned.tree$tip.label)
 
 
-plot(pruned.tree)
-
 ## removing discrepancies
 Data$Keep <- row.names(Data) %in% pruned.tree$tip.label
 Data <- Data[!(Data$Keep==FALSE),]
 
-
-plotTree(pruned.tree,type="fan",ftype="i")
 
 ## match species from csv to the tree
 neoplasia=dplyr::filter(Data, row.names(Data) %in% pruned.tree$tip.label)
@@ -189,26 +176,28 @@ fit
 fit$CI[1,]
 
 ## projection of the reconstruction onto the edges of the tree
-Aobj<-contMap(pruned.tree,neoplasia,plot=TRUE,res=200, type = "fan")
+Aobj<-contMap(pruned.tree,neoplasia,plot=FALSE,res=200, type = "fan")
 
 ## invert color scale so red is highest trait value and blue is lowest
 Aobj<-setMap(Aobj,invert=TRUE)
 
+## plot 
+s<-plot(Aobj,
+        fsize=c(1,0.8),leg.txt="neoplasia prevalence",lwd = 2.5,margin = 20)
+title(main="C", adj = .05, line = -1)
+
 ## plot multiple phylos on to one image
 
-layout(matrix(c(1,1,2,3), 2, 2, byrow = TRUE))
-par(mar=c(4, 4, 4, 4))
+layout(matrix(c(1,1,1,2,2,3), 6,1, byrow = TRUE))
 plot(Sobj,
-     fsize=c(0.3,0.8),leg.txt="neoplasia prevalence")
-title(main="Sauropsids", cex.main=1, line = -.5)
-par(mar=c(4, 4, 4, 4))
+     fsize=c(1,0.8),leg.txt="neoplasia prevalence")
+title(main="A", cex.main=3, line = -2, adj=.05)
 plot(Mobj,
-     fsize=c(0.4,0.8),leg.txt="neoplasia prevalence")
-title(main="Mammals", cex.main=1, line = -.5)
-par(mar=c(4, 4, 4, 4))
+    fsize=c(1,0.8),leg.txt="neoplasia prevalence")
+title(main="B", cex.main=3, line = -2, adj=.05)
 plot(Aobj,
-     fsize=c(0.4,0.8),leg.txt="neoplasia prevalence")
-title(main="Amphibians", cex.main=1, line = -.5)
+    fsize=c(1,0.8),leg.txt="neoplasia prevalence")
+title(main="C", cex.main=3, line = -2, adj=.05)
 
 
 
