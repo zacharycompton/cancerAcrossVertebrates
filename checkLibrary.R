58f836c3b606dfeff4d5e18306a466fdb58be165
wjmellon <94713257+wjmellon@users.noreply.github.com>
2023-11-09 12:39:27 -0700

    file to install needed libraries

---
+++ checkLibrary.R
@@ -0,0 +1,22 @@
+required_libraries <- c(
+  "ape", "nlme", "rms", "phytools",
+  "geiger", "caper", "tidyverse", "cowplot",
+  "ggrepel", "ggsci", "patchwork", "gee",
+  "poolr", "OUwie", "ggpubr", "gridGraphics",
+  "devtools", "ggtree", "plotrix", "picante",
+  "car", "gridExtra", "ggplot2", "grid",
+  "lemon", "sqldf"
+)
+
+# Function to check and install libraries
+install_if_missing <- function(lib) {
+  if (!requireNamespace(lib, quietly = TRUE)) {
+    install.packages(lib, dependencies = TRUE)
+    library(lib, character.only = TRUE)
+  } else {
+    message(paste("Library", lib, "is already installed."))
+  }
+}
+
+# Check and install required libraries
+invisible(sapply(required_libraries, install_if_missing))
